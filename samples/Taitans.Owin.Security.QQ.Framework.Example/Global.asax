7ebcee7a606f2934cfc4547a65a4fa72320dc4c7
Loongle <x120039483@hotmail.com>
2016-10-24 16:16:10 +0800

    Add a .NETFramworks sample of Taitans.Owin.Security.QQ

---
+++ samples/Taitans.Owin.Security.QQ.Framework.Example/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Taitans.Owin.Security.QQ.Framework.Example.MvcApplication" Language="C#" %>
---
+++ samples/Taitans.Owin.Security.QQ.Framework.Example/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Taitans.Owin.Security.QQ.Framework.Example.Startup))]
+namespace Taitans.Owin.Security.QQ.Framework.Example
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
